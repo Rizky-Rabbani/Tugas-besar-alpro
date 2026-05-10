48a5d62798e76a1869d5f1a6e3f5ccce479dbd1e
Rizky-Rabbani <121497999+Rizky-Rabbani@users.noreply.github.com>
2026-05-10 13:00:37 +0700

    Feat : Add constant and core feature to website.h

--- header/website.h
+++ header/website.h
@@ -1,14 +1,28 @@
 #ifndef WEBSITE_H
 #define WEBSITE_H
 
+#define CACHE_MAX_AMOUNT 10
+#define TABS_MAX_AMOUNT 10
+#define DOWNLOAD_MAX_AMOUNT 5
+#define MAX_WEB_PAGES 100
+
 typedef struct{
+    int id; 
+    char web_url[100];
+    char* content;
 
-    int x;
-    int y;
-}koordinat;
+}WebSite;
 
-void buattitik(koordinat *a, int x, int y);
+typedef struct{
+    WebSite Database[MAX_WEB_PAGES];
+    int matrix[MAX_WEB_PAGES][MAX_WEB_PAGES];
+    int website_count;
+}WebDatabase;
 
-float hitungjarak(koordinat a);
+void home(void);
+
+void CommandRouter1(char* command);
+
+void CommandRouter2(char* command, char* extra);
 
 #endif
