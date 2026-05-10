bc95cdecd8f5b3feadc8ff57e589dbcec59fb24c
Rizky-Rabbani <121497999+Rizky-Rabbani@users.noreply.github.com>
2026-05-10 11:35:18 +0700

    feat : add header file and data type

---
+++ header/website.h
@@ -0,0 +1,14 @@
+#ifndef WEBSITE_H
+#define WEBSITE_H
+
+typedef struct{
+
+    int x;
+    int y;
+}koordinat;
+
+void buattitik(koordinat *a, int x, int y);
+
+float hitungjarak(koordinat a);
+
+#endif
