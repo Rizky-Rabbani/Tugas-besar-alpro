bf767f0cc3673efc819c785504a2a8f71468e7e4
Rizky-Rabbani <121497999+Rizky-Rabbani@users.noreply.github.com>
2026-05-16 12:14:25 +0700

    Feat : add support for web management

--- src/main.c
+++ src/main.c
@@ -7,25 +7,22 @@ int main() {
     LoadConfig config;
     char *folder_data = "config"; // Nama folder tempat file CSV berada
 
-    // 1. Inisialisasi database agar kosong (isi 0)
+    // Inisialisasi database agar kosong (isi 0)
     InitWebDatabase(&db);
 
-    // 2. Load data pertama kali
+    // Load data pertama kali
     if (!LoadData(&db, &config, folder_data)) {
         printf("Gagal memuat data awal!\n");
         return 1;
     }
 
-    // Di main.c, setelah if (!LoadData(...))
-    printf("DEBUG: Website count: %d\n", db.website_count);
-    for(int i = 0; i < db.website_count; i++) {
-        printf("DEBUG: Index %d -> ID: %d, URL: '%s'\n", i, db.Database[i].id, db.Database[i].web_url);
-    }
+    // Inisialisasi Tab
+    InitTab(&db);
 
-    // 3. Masuk ke fungsi home
+    // Masuk ke fungsi home
     home(&db);
 
-    // 4. Bebaskan memori sebelum keluar (Sangat Penting!)
+    // Kalo udah selesai, Bebaskan memori sebelum keluar
     FreeWebDatabase(&db);
 
     return 0;
