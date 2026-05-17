28f2e8a448ae6fe4e851b4aa0521a97fb85ee854
Rizky A. Rabbani <121497999+Rizky-Rabbani@users.noreply.github.com>
2026-05-17 14:21:43 +0700

    Feat : add support for back <x> and forward <x>

--- src/Fungsi/F07_WebManagement.c
+++ src/Fungsi/F07_WebManagement.c
@@ -4,52 +4,63 @@
 #include <string.h>
 #include <stdlib.h>
 
-void back(WebDatabase* db) {
+void back(WebDatabase* db, int langkah)
+{
     int idx_tab_aktif = db->Tab.current_tab - 1;
     TabState *tab_aktif = &db->Tab.daftar_tab[idx_tab_aktif];
 
+    // Sedang di HOME dan tidak ada riwayat sebelumnya
     if (tab_aktif->current_web_idx == -1) 
     {
         printf("\nERROR: BACK TIDAK BISA DIGUNAKAN LAGI KARENA TIDAK ADA HALAMAN SEBELUMNYA!\n");
 
     }
-    else if (tab_aktif->current_web_idx == 0) 
+
+    // Langkah mundur melebihi batas indeks pertama (kembali ke HOME)
+    else if (tab_aktif->current_web_idx - langkah < 0) 
     {
+        // Ubah halaman menjadi di HOME
         tab_aktif->current_web_idx = -1; 
-        printf("\nBACK : Kembali ke website HOME\n");
+        printf("\nBACK : Kembali ke HOME\n");
 
     }
+
+    // Mundur beberapa langkah ke website sebelumnya
     else 
     {
-        tab_aktif->current_web_idx--; 
+        tab_aktif->current_web_idx -= langkah; // Mundur sebanyak X langkah
         int idx_web = tab_aktif->current_web_idx;
         
         printf("\nBACK : Kembali ke website %s\n", tab_aktif->daftar_web[idx_web].web_url);
         printf("%s\n", tab_aktif->daftar_web[idx_web].content);
-
     }
 }
 
-void forward(WebDatabase* db) {
+void forward(WebDatabase* db, int langkah) 
+{
     int idx_tab_aktif = db->Tab.current_tab - 1;
     TabState *tab_aktif = &db->Tab.daftar_tab[idx_tab_aktif];
 
-    if (tab_aktif->current_web_idx >= tab_aktif->web_count - 1) 
+    // Langkah maju melebihi jumlah halaman yang ada di riwayat depan
+    if (tab_aktif->current_web_idx + langkah >= tab_aktif->web_count) 
     {
         printf("\nERROR: FORWARD TIDAK BISA DIJALANKAN KARENA TIDAK ADA HALAMAN SELANJUTNYA!\n");
         
         if (tab_aktif->web_count > 0) {
-            int idx_akhir = tab_aktif->current_web_idx;
+            int idx_akhir = tab_aktif->web_count - 1; // Langsung arahkan ke web paling ujung
+            tab_aktif->current_web_idx = idx_akhir;
             printf("\n(Konten web paling akhir: %s)\n", tab_aktif->daftar_web[idx_akhir].web_url);
             printf("%s\n", tab_aktif->daftar_web[idx_akhir].content);
         } else {
             printf("\n(Konten web paling akhir: HOME)\n");
         }
-        
+
     }
+
+    // Bisa maju beberapa langkah ke depan
     else 
     {
-        tab_aktif->current_web_idx++; // Maju 1 indeks
+        tab_aktif->current_web_idx += langkah; // Maju sebanyak X langkah
         int idx_web = tab_aktif->current_web_idx;
         
         printf("\nFORWARD : Maju ke website %s\n", tab_aktif->daftar_web[idx_web].web_url);
@@ -57,5 +68,3 @@ void forward(WebDatabase* db) {
 
     }
 }
-
-
