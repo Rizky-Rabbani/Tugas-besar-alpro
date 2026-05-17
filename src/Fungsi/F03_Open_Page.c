ee0c904b1ffdd33fd55537c79994a152a51c1220
Rizky A. Rabbani <121497999+Rizky-Rabbani@users.noreply.github.com>
2026-05-17 14:49:05 +0700

    Feat : Add support for F09 Linked Pages

--- src/Fungsi/F03_Open_Page.c
+++ src/Fungsi/F03_Open_Page.c
@@ -5,18 +5,45 @@
 #include <stdlib.h>
 
 void open(WebDatabase *db, char* link) {
-    int i;
+    int i, j;
+    int found = 0;
 
-    for(i = 0; i < 9; i++) {
-
+    for(i = 0; i < db->website_count; i++) {
         if(strcmp(link, db->Database[i].web_url) == 0) 
         {
+            // Fungsi open utama
             printf("\n%s\n", db->Database[i].content);
-
             InputTab(db, db->Database[i]);
+
             
+            found = 1;
+            
+            // Fungsi linkedpages
+            printf("\nLinked pages :\n");
+            int nomor_tautan = 1;
+            for(j = 0; j < db->website_count; j++) 
+            {
+                if(db->matrix[i][j] == 1) {
+                    printf("[%d] %s\n", nomor_tautan, db->Database[j].web_url);
+                    nomor_tautan++;
+                    
+                }
+                
+            }
+
+            if(nomor_tautan == 1) 
+            {
+                printf("(Tidak ada website yang tertaut)\n");
+                
+            }
             return;
+            
         }
     }
-    printf("URL %s tidak ditemukan.\n", link);
+    
+    if(!found)
+    {
+        printf("URL %s tidak ditemukan.\n", link);
+        
+    }
 }
