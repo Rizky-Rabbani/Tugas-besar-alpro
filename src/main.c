dabbbde83135ad6852ded51787b3d449740c28c0
Rizky A. Rabbani <121497999+Rizky-Rabbani@users.noreply.github.com>
2026-05-18 14:12:51 +0700

    reverse main.c

--- src/main.c
+++ src/main.c
@@ -2,31 +2,27 @@
 #include "website.h"
 #include <stdio.h>
 
-extern void SeedLCG(); 
-
 int main() {
     WebDatabase db;
     LoadConfig config;
     char *folder_data = "config"; // Nama folder tempat file CSV berada
 
-    // Inisialisasi database agar kosong (isi 0)
+    // 1. Inisialisasi database agar kosong (isi 0)
     InitWebDatabase(&db);
 
-    // Load data pertama kali
+    // 2. Load data pertama kali
     if (!LoadData(&db, &config, folder_data)) {
         printf("Gagal memuat data awal!\n");
         return 1;
     }
 
-    SeedLCG();
-
     // Inisialisasi Tab
     InitTab(&db);
 
-    // Masuk ke fungsi home
+    // 3. Masuk ke fungsi home
     home(&db);
 
-    // Kalo udah selesai, Bebaskan memori sebelum keluar
+    // 4. Bebaskan memori sebelum keluar (Sangat Penting!)
     FreeWebDatabase(&db);
 
     return 0;
